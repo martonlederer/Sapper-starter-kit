d8f216dd545f4672a7fa4f1bd54def0ae3241a04
Marton <marton@lederer.hu>
2020-08-14 11:26:36 +0200

    Remove counter

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,7 @@
-<script context="module" lang="ts">
-	export async function preload(page, session) {
-		console.log('preload!', page, session)
-	}
-</script>
-
 <script lang="ts">
-	import { onMount } from 'svelte'
-	import { writable } from 'svelte/store'
-
-  import Counter from '@app/components/Counter.svelte'
   
-	/**
- 	*! Adding types to variables breaks preloading
- 	*  TODO: Figure out why...
-	*/
-	//const count: Writable<number> = writable(100)
-	const count = writable(100)
+  let test: string = 'test'
 
-	onMount(() => {
-		console.log('App mounted')
-	})
 </script>
 
 <svelte:head>
@@ -27,23 +9,15 @@
 </svelte:head>
 
 <div class="page test">
-	<h1>Great success!</h1>
-
+	<h1>Great success! {test}</h1>
 	<figure>
 		<img alt="Borat" src="great-success.png" />
 		<figcaption>HIGH FIVE!</figcaption>
 	</figure>
-
 	<p>
 		<strong>
 			Try editing this file (src/routes/index.svelte) to test live reloading.
 		</strong>
-	</p>
-
-	<p>
-		<Counter />
-		<Counter value={1}>Counter 1</Counter>
-		<Counter value={$count} step={3}>Counter 2</Counter>
 	</p>
 </div>
 
